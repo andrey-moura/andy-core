455681eded28b72dac9bc7f8bcffab1a56273e79
Moonslate <Andreymds88@gmail.com>
2023-02-11 14:53:28 -0300

    fix wrong preprocessor

--- core.hpp
+++ core.hpp
@@ -128,7 +128,7 @@ namespace uva
         public:
             void* m_value_ptr = nullptr;
             //for debugging
-#ifndef UVA_DEBUG_LEVEL > 1
+#if __UVA_DEBUG_LEVEL__ > 1
             integer_type* m_integer_ptr = nullptr;
             real_type* m_real_ptr = nullptr;
             string_type* m_string_ptr = nullptr;
