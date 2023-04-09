731e406a54f9406b4ceb6934f63e24daadc3a3d8
Andrey Moura <andreymds88@gmail.com>
2023-04-09 12:54:20 -0300

    [WIP] Add some code needed by uva-engine

--- color.hpp
+++ color.hpp
@@ -4,9 +4,17 @@
 
 namespace uva
 {
+    struct color24
+    {
+        unsigned char r;
+        unsigned char g;
+        unsigned char b;
+    };
     template<typename T>
     struct basic_color
     {
+        basic_color() = default;
+
         basic_color(const basic_color& other)
             : r(other.r), g(other.g), b(other.b), a(other.a)
         {
@@ -19,11 +27,33 @@ namespace uva
 
         }
 
-        T r;
-        T g;
-        T b;
-        T a;
+        union {
+            uint32_t rgba;
+
+            struct {
+                T r;
+                T g;
+                T b;
+                T a;
+            };
+        };
+
+        bool operator==(const basic_color<T>& other) const {
+            return rgba == other.rgba;
+        }
+        bool operator!=(const basic_color<T>& other) const {
+            return rgba != other.rgba;
+        }
+        basic_color<T>& operator=(const basic_color<T>& other) {
+            rgba = other.rgba;
+            return *this;
+        }
+        bool operator<(const basic_color<T>& c) const
+        {
+            return rgba < c.rgba;
+        }
     };
 
     using color = basic_color<uint8_t>;
+    using color32 = basic_color<uint8_t>;
 };
