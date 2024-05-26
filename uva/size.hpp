500a9b0d7ea8573e45f2bbf2d12e206f3f44822b
Andrey Moura <andreymds88@gmail.com>
2024-05-26 12:56:23 -0300

    Copy size from engine to core

---
+++ uva/size.hpp
@@ -0,0 +1,63 @@
+#pragma once
+
+namespace uva {
+
+    template<typename T>
+    struct basic_size
+    {
+        T w;
+        T h;
+
+        basic_size() = default;
+
+        basic_size(const T& __w, const T& __h)
+            : w(__w), h(__h)
+        {
+
+        }
+
+        T area() const { return w * h; }
+        template<typename OtherT>
+        basic_size<T>& operator=(const basic_size<OtherT>& other) {
+            w = (T)other.w;
+            h = (T)other.h;
+            return *this;
+        }
+        basic_size<T>& operator+=(const basic_size<T>& other) {
+            w += other.w;
+            h += other.h;
+            return *this;
+        }
+
+        basic_size<T>& operator-=(const basic_size<T>& other) {
+            w -= other.w;
+            h -= other.h;
+            return *this;
+        }
+        basic_size<T>& operator*=(int x) {
+            w *= x;
+            h *= x;
+            return *this;
+        }
+        basic_size<T> operator*(int x) {
+            return { w*(T)x, h*(T)x };
+        }
+        basic_size<T>& operator=(const basic_size<T>& other) {
+            w = other.w;
+            h = other.h;
+            return *this;
+        }
+        bool operator==(const basic_size<T>& other) const {
+            return other.w == w && other.h == h;
+        }
+        bool operator<(const basic_size<T>& other) const {
+            return w < other.w && h < other.h;
+        }
+        bool operator<=(const basic_size<T>& other) const {
+            return w <= other.w && h <= other.h;
+        }
+    };
+
+    typedef basic_size<int> size;
+    typedef basic_size<float> sizef;
+}
